283c91138229ddb12fabc8026687f3f9e7e0d13b
Moralescr <moralesguadamuz@gmail.com>
2020-09-06 03:38:58 +0000

    fix

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Biscolab\ReCaptcha\ReCaptchaServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -174,7 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Biscolab\ReCaptcha\ReCaptchaServiceProvider::class,
 
     ],
 
